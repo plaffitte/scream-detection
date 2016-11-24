fc1d7c1f5f8d98ba76931cf650f2669d0911da19
piero <pl.laffitte@gmail.com>
2016-11-24 16:55:32 +0100

    upgrading with to computation capabilities

--- format_pickle_data3.py
+++ format_pickle_data3.py
@@ -2,7 +2,7 @@ import os as os
 import scikits.audiolab as audlab
 import cPickle, gzip, sys
 import numpy as np
-import shutil
+import math, shutil
 from spectral import get_mfcc
 from util_func import parse_arguments, parse_classes
 
@@ -20,20 +20,21 @@ N = int(arguments['N'])
 slide = int(arguments['slide'])
 exp_path = arguments['exp_path']
 threshold = int(arguments['threshold'])
-
-shutil.copyfile('/home/piero/Documents/Scripts/format_pickle_data3.py',
-                os.path.join(exp_path,'format_pickle_data3.py'))
+compute_delta = arguments['deltas']
+shutil.copyfile('/home/piero/Documents/Scripts/format_pickle_data3.py', os.path.join(exp_path,'format_pickle_data3.py'))
 N_classes = len(classes)
 label_dic = {}
 for k in range(N_classes):
     label_dic[classes[k]] = k
 initial_path = '/home/piero/Documents/Speech_databases/DeGIV/29-30-Jan/'+name_var+'_labels' # label files
-target_path = os.path.join(exp_path,"data")
+target_path = os.path.join(exp_path,'data')
 os.chdir(initial_path)
 cur_dir = os.getcwd()
 file_list = os.listdir(cur_dir)
 wav_dir = os.path.join(os.path.split(initial_path)[0], 'wav')
 label_vector = np.zeros(1, dtype=np.float32)
+if compute_delta == "True":
+    size = 2 * size
 data_vector = np.zeros((1, size * N), dtype=np.float32)
 time_per_occurrence_class = [[] for i in range(N_classes)]
 logfile = os.path.join(target_path, 'data_log_'+name_var+'.log')
@@ -41,12 +42,12 @@ log = open(logfile, 'w')
 time = 0
 
 for i in range(len(file_list)):
-    lab_name = file_list[i] # os.path.split(os.path.join(wav_dir,file_list[i]))[1]
+    lab_name = file_list[i] #os.path.split(os.path.join(wav_dir,file_list[i]))[1]
     print("-->> Reading file:", lab_name)
     if '~' in lab_name:
         continue
-    with open(os.path.join(cur_dir, file_list[i]), 'r') as f:
-        lines = f.readlines()
+    with open(os.path.join(cur_dir, lab_name), 'r') as lab_f:
+        lines = lab_f.readlines()
         if "WS" in lab_name:
             wave_name = os.path.join(wav_dir, lab_name[:-7]+'.wav')
         else:
@@ -65,44 +66,54 @@ for i in range(len(file_list)):
                     length = (stop - start) / 10.0 ** 7
                 audio = f.read_frames(freq * length)
                 if label in label_dic:
-                    time_per_occurrence_class[label_dic[label]].append(length)
-                    time = np.sum(time_per_occurrence_class[label_dic[label]])
                     if time < threshold:
                         # energy = np.sum(audio ** 2, 0) / len(audio)
                         signal = audio  # audio/math.sqrt(energy)
                         mfcc = get_mfcc(signal, freq, winstep=window_step, winlen=window_size, nfft=2048, lowfreq=lowfreq,
                                         highfreq=highfreq, numcep=size, nfilt=size + 2)
+                        if compute_delta == "True":
+                            d1_mfcc = np.zeros((mfcc.shape[0]-1,mfcc.shape[1]))
+                            for k in range(mfcc.shape[0]-1):
+                                d1_mfcc[k,:] = mfcc[k+1,:] - mfcc[k,:]
+                            mfcc = mfcc[1:,:]
                         N_iter = np.floor((len(mfcc) - N) / slide)
                         # apply context window
                         if (length/window_step) > N:
+                            time_per_occurrence_class[label_dic[label]].append(length)
+                            time = np.sum(time_per_occurrence_class[label_dic[label]])
                             mfcc_matrix = np.zeros((1, size * N))
                             for k in range(int(N_iter)):
                                 mfcc_vec = []
                                 for kk in range(N):
                                     mfcc_vec = np.concatenate((mfcc_vec, mfcc[k * slide + kk, :]))
+                                    if compute_delta == "True":
+                                        mfcc_vec = np.concatenate((mfcc_vec, d1_mfcc[k * slide + kk, :]))
                                 mfcc_matrix = np.concatenate((mfcc_matrix, mfcc_vec[np.newaxis, :]))
-                            # get the numeric label corresponding to the literal label
                             num_label = label_dic[label] * np.ones(len(mfcc_matrix) - 1)
                             label_vector = np.append(label_vector, num_label.astype(np.float32, copy=False))
-                            data_vector = np.concatenate((data_vector, mfcc_matrix[1:, :].astype(np.float32, copy=False)), 0)
+                            data_vector = np.concatenate((data_vector, mfcc_matrix[1:,:].astype(np.float32, copy=False)),0)
                         else:
                             print('Input data sequence does not match minimal length requirement: ignoring')
-                # else:
-                #     del audio
+                else:
+                    del audio
             except KeyError, e:
                 print "Wrong label name:", label, "at line", j+1
             except:
                 print "Unexpected error:", sys.exc_info()[0]
                 raise
     print("Size of data_vector: ", data_vector.shape)
-data_vector = data_vector[1:, :]
+data_vector = data_vector[1:,:]
 label_vector = label_vector[1:]
+# Feature Standardization
+#data_vector = preproc.scale(data_vector)
+
 total_L_sec = stop / (10.0 ** 7)
 total_N = total_L_sec/window_step
 obj = [data_vector, label_vector]
 # Now write to file, for pdnn learning
-target_name = os.path.join(target_path, name_var+'.pickle.gz')
-cPickle.dump(obj, gzip.open(target_name, 'wb'), cPickle.HIGHEST_PROTOCOL)
+target_name = os.path.join(target_path,name_var+'.pickle.gz')
+cPickle.dump(obj, gzip.open(target_name,'wb'),cPickle.HIGHEST_PROTOCOL)
+
 for class_name, class_value in label_dic.items():
     string = 'Name of corresponding wav file:'+wave_name+'\n'
     log.write(string)
