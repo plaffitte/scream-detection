b7e2e72785e9013471588612bd79a684042fc62f
piero <pl.laffitte@gmail.com>
2016-12-01 19:53:44 +0100

    adding text_to_latex utility function in utils script, and updating class parsing function to match multi env

--- util_func.py
+++ util_func.py
@@ -13,6 +13,20 @@ def parse_classes(class_str):
         if not class_str[i+1] is '}':
             i += 1
         start = i + 1
+    i += 1
+    if i < len(class_str) - 1:
+        if class_str[i+1] is '+':
+            i += 2
+            start = i + 1
+            while not class_str[i+1] is '}':
+                while class_str[i+1] not in {',', '}'}:
+                    i += 1
+                end = i+1
+                class_list.append(class_str[start:end])
+                if not class_str[i+1] is '}':
+                    i += 1
+                start = i + 1
+
     return class_list
 
 def parse_arguments(arg_elements):
@@ -26,3 +40,38 @@ def parse_arguments(arg_elements):
 def log(string):
     sys.stderr.write('[' + str(datetime.now()) + '] ' + str(string) + '\n')
     print('[' + str(datetime.now()) + '] ' + str(string) + '\n')
+
+def text_to_latex(results,class_list,path):
+    no_classes = len(class_list)
+    text = '\\begin{table}[h*]\n'
+    text = text + '\\centering\n'
+    text = text + '\\begin{tabular}{'
+    for i in range(no_classes+2):
+        text = text + '|c'
+    text = text + '}\n'
+    text = text + '\\hline\n'
+    for i in range(no_classes):
+        text  = text + '&' + '\\textbf{' + class_list[i] + '}'
+    text = text + '&' + 'Total error' + '\\' + '\\' + '\n'
+    text = text + '\\hline\n'
+    for i in range(no_classes):
+        text = text + '\\textbf{' + class_list[i] + '}'
+        for j in range(no_classes):
+            if i==j:
+                text = text + '&' + '\\cellcolor{lightgray}' + '\\textbf{ ' + str(results[i,j]) + '}'
+            else:
+                text = text + '& ' + str(results[i,j])
+
+        text = text + ' \\' + '\\' + '\n'
+        text = text + '\\hline\n'
+
+    text = text + '\\end{tabular}'
+    text = text + '\\caption{'
+    for i in range(no_classes-1):
+        text = text + class_list[i] + ' vs '
+    text = text + class_list[no_classes-1]
+    text = text + '}\n' + '\\end{table} \n'
+    logfile = path + '/results.tex'
+    log = open(logfile, 'w')
+    log.write(text)
+    log.close()
