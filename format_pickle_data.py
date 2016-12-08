065442acbd70e5836aa826654fde7ab896ed6469
piero <pl.laffitte@gmail.com>
2016-12-08 19:25:54 +0100

    upgrading to match svn version

--- format_pickle_data.py
+++ format_pickle_data.py
@@ -6,48 +6,73 @@ import math, shutil
 from spectral import get_mfcc
 from util_func import parse_arguments, parse_classes
 
+typeFeature = "MFCC"
+name_cur_file = os.path.basename(__file__)
+name_cur_dir = os.path.dirname(os.path.abspath(__file__))
+source_dir = os.getcwd()
+
+##### Parse Arguments #####
 arg_elements = [sys.argv[i] for i in range(1, len(sys.argv))]
 arguments = parse_arguments(arg_elements)
 name_var = arguments['data_type']
+target_path = arguments['rep_test']
+param_str = arguments['param']
 classes = parse_classes(arguments['classes'])
-# name_var = 'test'
-window_step = float(arguments['window_step'])
-window_size = float(arguments['window_size'])
-highfreq = int(arguments['highfreq'])
-lowfreq = int(arguments['lowfreq'])
-size = int(arguments['size'])
-N = int(arguments['N'])
-slide = int(arguments['slide'])
-exp_path = arguments['exp_path']
-threshold = int(arguments['threshold'])
-compute_delta = arguments['deltas']
-shutil.copyfile('/home/piero/Documents/Scripts/format_pickle_data.py',
-                os.path.join(exp_path,'format_pickle_data.py'))
 N_classes = len(classes)
+param_list = parse_classes(param_str)
+window_step=float(param_list[0] )# in seconds, hop size between two successive mfcc windows
+window_size=float(param_list[1] )# in seconds, size of MFCC window
+highfreq=float(param_list[2]) # maximal analysis frequency for mfcc
+lowfreq=float(param_list[3]) # minimal analysis frequency for mfcc
+size=int(param_list[4]) # number of mfcc coef
+N=int(param_list[5]) #contextual window
+slide=int(param_list[6])
+threshold = int(param_list[7])
+compute_delta = param_list[8]
+
 label_dic = {}
-for k in range(N_classes):
-    label_dic[classes[k]] = k
-initial_path = '/home/piero/Documents/Speech_databases/DeGIV/29-30-Jan/'+name_var+'_labels' # label files
-target_path = os.path.join(exp_path,'data')
-os.chdir(initial_path)
-cur_dir = os.getcwd()
-file_list = os.listdir(cur_dir)
-wav_dir = os.path.join(os.path.split(initial_path)[0], 'wav')
+for i in range(len(classes)):
+    label_dic[classes[i]] = i
+    print classes[i] + " : " + str(i)
+
+##### Copy label files and current script where necessary to trace experiment #####
+shutil.copyfile(os.path.join(name_cur_dir, name_cur_file), os.path.join(target_path, name_cur_file))
+label_path = "/home/piero/Documents/Speech_databases/DeGIV/29-30-Jan/" + name_var + '_labels' # Path to label files
+shutil.rmtree(os.path.join(target_path, name_var+'_labels'), ignore_errors=True)
+shutil.copytree(label_path, os.path.join(target_path, name_var+'_labels'))
+
+##### Memory allocation #####
 label_vector = np.zeros(1, dtype=np.float32)
 if compute_delta == "True":
     size = 2 * size
 data_vector = np.zeros((1, size * N), dtype=np.float32)
 time_per_occurrence_class = [[] for i in range(N_classes)]
+
+##### Couple log writings #####
 logfile = os.path.join(target_path, 'data_log_'+name_var+'.log')
 log = open(logfile, 'w')
-time = 0
+string = '===== Parametre Features:\n'; log.write(string)
+string = ' typeFeature : ' + typeFeature + '\n'; log.write(string)
+string = ' window_step : ' + param_list[0] + '\n'; log.write(string)
+string = ' window_size : ' + param_list[1] + '\n'; log.write(string)
+string = ' highfreq : ' + param_list[2] + '\n'; log.write(string)
+string = ' lowfreq : ' + param_list[3] + '\n'; log.write(string)
+string = ' size : ' + param_list[4] + '\n'; log.write(string)
+string = ' N contextual window : ' + param_list[5] + '\n'; log.write(string)
+string = ' Slide : ' + param_list[6] + '\n\n'; log.write(string)
+string = '===== Name of corresponding wav file:\n'; log.write(string)
 
+##### Main Loop #####
+file_list = os.listdir(label_path)
+file_list = [file for file in file_list if os.path.isfile(os.path.join(label_path, file))]
+wav_dir = os.path.join(os.path.split(label_path)[0], 'wav')
+time = 0
 for i in range(len(file_list)):
     lab_name = file_list[i] #os.path.split(os.path.join(wav_dir,file_list[i]))[1]
     print("-->> Reading file:", lab_name)
     if '~' in lab_name:
         continue
-    with open(os.path.join(cur_dir, lab_name), 'r') as lab_f:
+    with open(os.path.join(label_path, file_list[i]), 'r') as lab_f:
         lines = lab_f.readlines()
         if "WS" in lab_name:
             wave_name = os.path.join(wav_dir, lab_name[:-7]+'.wav')
@@ -103,29 +128,36 @@ for i in range(len(file_list)):
                 print "Unexpected error:", sys.exc_info()[0]
                 raise
     print("Size of data_vector: ", data_vector.shape)
+
 data_vector = data_vector[1:,:]
 label_vector = label_vector[1:]
 # Feature Standardization
 #data_vector = preproc.scale(data_vector)
-
 total_L_sec = stop / (10.0 ** 7)
 total_N = total_L_sec/window_step
 obj = [data_vector, label_vector]
 # Now write to file, for pdnn learning
-target_name = os.path.join(target_path,name_var+'.pickle.gz')
+target_name = os.path.join(target_path,"data/"+name_var+'.pickle.gz')
 cPickle.dump(obj, gzip.open(target_name,'wb'),cPickle.HIGHEST_PROTOCOL)
 
+string = '\n======= data description:\n'
+log.write(string)
+
 for class_name, class_value in label_dic.items():
-    string = 'Name of corresponding wav file:'+wave_name+'\n'
-    log.write(string)
-    string = 'number of data from class' + class_name + ':' + str(len(time_per_occurrence_class[class_value]))+'\n'
-    log.write(string)
-    string = 'length of smallest data from class:' + class_name + ':' + str(min(time_per_occurrence_class[class_value]))+'\n'
-    log.write(string)
-    string = 'length of longest data from class:' + class_name + ':' + str(max(time_per_occurrence_class[class_value]))+'\n'
-    log.write(string)
-    string = 'mean length of data from class:' + class_name + ':' + str(np.mean(time_per_occurrence_class[class_value]))+'\n'
-    log.write(string)
-    string = 'total length of data from class:' + class_name + ':' + str(np.sum(time_per_occurrence_class[class_value]))+'\n'
-    log.write(string)
+    try:
+    # if not time_per_occurrence_class[class_value]:
+        string = 'Name of corresponding wav file:'+wave_name+'\n'
+        log.write(string)
+        string = 'number of data from class' + class_name + ':' + str(len(time_per_occurrence_class[class_value]))+'\n'
+        log.write(string)
+        string = 'length of smallest data from class:' + class_name + ':' + str(min(time_per_occurrence_class[class_value]))+'\n'
+        log.write(string)
+        string = 'length of longest data from class:' + class_name + ':' + str(max(time_per_occurrence_class[class_value]))+'\n'
+        log.write(string)
+        string = 'mean length of data from class:' + class_name + ':' + str(np.mean(time_per_occurrence_class[class_value]))+'\n'
+        log.write(string)
+        string = 'total length of data from class:' + class_name + ':' + str(np.sum(time_per_occurrence_class[class_value]))+'\n'
+        log.write(string)
+    except:
+        continue
 log.close()
